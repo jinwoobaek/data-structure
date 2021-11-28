754c0b27d0bf9be9defb1280294cdbc11adcf21f
Jinwoo-Home <back3946@gmail.com>
2021-11-28 15:17:24 +0900

    Practice Stack

---
+++ src/com/stury/stack/PracticeStack.java
@@ -0,0 +1,21 @@
+package com.stury.stack;
+
+import java.util.ArrayList;
+
+public class PracticeStack<T> {
+    private ArrayList<T> stack = new ArrayList<T>();
+
+    public void push(T item) {
+        stack.add(item);
+    }
+
+    public T pop() {
+        if(stack.isEmpty())
+            return null;
+        return stack.remove(stack.size()-1);
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
