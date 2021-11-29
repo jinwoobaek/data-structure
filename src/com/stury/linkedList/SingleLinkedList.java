83cf6264684b9d9119608c132cdedfd8ee9b41f3
Jinwoo-Home <back3946@gmail.com>
2021-11-29 16:21:55 +0900

    Single Linked List (2)  - Add Node

--- src/com/stury/linkedList/SingleLinkedList.java
+++ src/com/stury/linkedList/SingleLinkedList.java
@@ -29,13 +29,49 @@ public class SingleLinkedList<T> {
 
     public void printAll() {
         // Not empty list
-        if(head != null) {
+        if (head != null) {
             Node<T> node = this.head;
             System.out.println(node.data);
-            while (node.next != null){
+            while (node.next != null) {
                 node = node.next;
                 System.out.println(node.data);
             }
+        }
+    }
+
+    public Node<T> search(T data) {
+        if (head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while (node != null) {
+                if (node.data == data) {
+                    return node;
+                } else {
+                    node = node.next;
+                }
+
+            }
+            // 검색 결과 해당 data를 가진 노드가 없을 경우
+            return null;
+        }
+    }
+
+    public void addNodeInside(T data, T isData) {
+        // isData가 존재하는 노드 뒤에 새로운 노드 삽입
+        Node<T> searchedNode = this.search(isData);
+
+        // 검색된 노드가 없다면 맨 뒤 노드에 노드 삽입
+        if (searchedNode == null) {
+            this.addNode(data);
+        }
+        // 검색된 노드가 존재하면 그 뒤에 삽입
+        else {
+            // 검색된 노드의 뒤 노드를 미리 저장
+            Node<T> nextNode = searchedNode.next;
+            searchedNode.next = new Node<T>(data);
+            // 새로 추가한 노드의 뒤에 기존 nextNode 추가
+            searchedNode.next.next = nextNode;
         }
     }
 
